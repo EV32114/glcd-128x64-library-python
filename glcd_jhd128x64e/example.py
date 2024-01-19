b2eec8409da69aaca5c105f0fb8cd8e22bf5a3c6
Evyatar Meshulam <evyatarmesh@gmail.com>
2024-01-19 14:38:29 +0200

    change LCD pinout

--- glcd_jhd128x64e/example.py
+++ glcd_jhd128x64e/example.py
@@ -1,22 +1,22 @@
 import jhd_glcd  # glcd_jhd128x64e
 import time
 import numpy as np
-      
+
+
+def print_menu(GLCD):
+    GLCD.set_cursor(0,0,0)
+    GLCD.print_str("Please choose a menu:\n\n")
+    GLCD.print_str("# Show weapon list\n", True)
+    GLCD.print_str("# Show all logs")
+
+
 def main():
-    GLCD = jhd_glcd.KS0108(rs=4, rw=7, en=8, d0=9, d1=10, 
-                              d2=11, d3=14, d4=15, d5=17, 
+    GLCD = jhd_glcd.KS0108(rs=4, rw=21, en=7, d0=12, d1=16, 
+                              d2=26, d3=14, d4=15, d5=17, 
                               d6=18, d7=25, chip_set0=22, 
                               chip_set1=23, reset=24)
     GLCD.start()
-    GLCD.set_cursor(0,0,0)
-    # GLCD.data_write(0b00000001, 1)
-    print(GLCD.Line_Num, GLCD.Cursor_Pos)
-    GLCD.draw_line(0,5,128,5)
-    print(GLCD.mat[5])
-    #GLCD.draw_line(45, 0, 45, 63)
-    #GLCD.set_cursor(0,0,50)
-    #GLCD.print_str("Logs")
-    #GLCD.draw_line(76,0,76,63)
+    print_menu(GLCD)
         
             
 if __name__ == "__main__":
